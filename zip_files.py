a582f140642e32adf07bae9009da05522594c77a
Daniel Zyss <d.zyss15@ejm.org>
2020-02-28 22:22:27 +0000

    changed zip arcname

--- zip_files.py
+++ zip_files.py
@@ -4,4 +4,4 @@ import os
 for name in os.listdir("exports"):
     if name[-3:]=="txt":
         with ZipFile("zip-exports/"+name[:-4]+".zip", "w") as newzip:
-            newzip.write("exports/"+name)
+            newzip.write("exports/"+name, arcname="predictions.txt")
